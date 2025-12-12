09bccacd4b8661818befb73ccac5ba427497e04f
rusja11-87 <rusja11.8797@gmail.com>
2025-12-12 13:50:10 +0300

    Создал основу проекта и gitignore

---
+++ PetProjectNotebook/PetProjectNotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetProjectNotebookApp.swift
+//  PetProjectNotebook
+//
+//  Created by Руслан Плешкунов on 12.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct PetProjectNotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
